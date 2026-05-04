5eecd587f4898e9485f5b107f55421ab1ddd06a0
Samuel Adams <sama29571@gmail.com>
2026-05-04 09:07:51 +0000

    chore: add more pipelines

--- hugging face/1_pipeline.py
+++ hugging face/1_pipeline.py
@@ -32,3 +32,27 @@ filtered = wikipedia.filter(lambda row: "football" in row["text"])
 example = filtered.select(range(1))
 
 print(example[0]["text"])
+
+
+# available tasks are ['audio-classification', 'automatic-speech-recognition', 'depth-estimation', 'document-question-answering', 'feature-extraction', 'fill-mask', 'image-classification', 'image-feature-extraction', 'image-segmentation', 'image-text-to-text', 'image-to-image', 'image-to-text', 'mask-generation', 'ner', 'object-detection', 'question-answering', 'sentiment-analysis', 'summarization', 'table-question-answering', 'text-classification', 'text-generation', 'text-to-audio', 'text-to-speech', 'text2text-generation', 'token-classification', 'translation', 'video-classification', 'visual-question-answering', 'vqa', 'zero-shot-audio-classification', 'zero-shot-classification', 'zero-shot-image-classification', 'zero-shot-object-detection', 'translation_XX_to_YY']
+#grammar checker
+# Create a pipeline for grammar checking
+grammar_checker = pipeline(
+  task="text-classification", 
+  model="abdulmatinomotoso/English_Grammar_Checker"
+)
+
+# Check grammar of the input text
+output = grammar_checker("I will walk dog")
+print(output)
+
+
+
+#QNLI - Question Natural Language Inference. It is a popular dataset and task used in machine learning to evaluate the performance of natural language processing (NLP) models, specifically in understanding the relationship between a question and a context sentence.
+# Create the pipeline
+classifier = pipeline(task="text-classification", model="cross-encoder/qnli-electra-base")
+
+# Predict the output
+output = classifier("Where is the capital of France?, Brittany is known for its stunning coastline.")
+
+print(output)
