246147c32fa65e5d7838f4fe0ff34a0fd306379f
Samuel Adams <sama29571@gmail.com>
2026-05-06 15:43:21 +0000

    chore: add response format

--- hugging face/1_pipeline.py
+++ hugging face/1_pipeline.py
@@ -234,3 +234,20 @@ response= client.chat.completions.create(
 
 # Print the response
 print(response.choices[0].message.tool_calls[0].function.arguments)
+
+
+#specifying response format
+# Create the OpenAI client
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Create the request
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+   {"role": "user", "content": "I have these notes with book titles and authors: New releases this week! The Beholders by Hester Musson, The Mystery Guest by Nita Prose. Please organize the titles and authors in a json file."}
+  ],
+  response_format = {"type":"json_object"}
+)
+
+# Print the response
+print(response)
