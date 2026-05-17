f1996550576fca2cfcbd70c897f2d9af1c51f8e5
Samuel Adams <sama29571@gmail.com>
2026-05-17 15:56:03 +0000

    chore: updates

--- embeddings.py
+++ embeddings.py
@@ -44,6 +44,7 @@ print(response_dict)
 
 #now we need to reduce the 1536-dimensional embedding vector to a lower dimension for visualization or further processing. We can use techniques like PCA (Principal Component Analysis) or t-SNE (t-distributed Stochastic Neighbor Embedding) for this purpose.
 
+# Reduce the number of embeddings dimensions from 1,536 to two using the tsne model provided.
 # Create reviews and embeddings lists using list comprehensions
 categories = [product['category'] for product in products]
 embeddings = [product['embedding'] for product in products]
@@ -51,3 +52,34 @@ embeddings = [product['embedding'] for product in products]
 # Reduce the number of embeddings dimensions to two using t-SNE
 tsne = TSNE(n_components=2, perplexity=5)
 embeddings_2d = tsne.fit_transform(np.array(embeddings))
+
+
+def create_embeddings(texts):
+  response = client.embeddings.create(
+    model="text-embedding-3-small",
+    input=texts
+  )
+  response_dict = response.model_dump()
+  
+  return [data['embedding'] for data in response_dict['data']]
+
+
+# Finding the most similar product
+# Being able to compute similarity between embeddings is a key step within embeddings applications. In this exercise, you'll return to the products list of dictionaries that you worked with previously, which contains the embedded short descriptions you also created earlier.
+
+# You'll compare a piece of text to these embedded descriptions to identify the most similar description.
+# Embed the search text
+search_text = "soap"
+search_embedding = create_embeddings(search_text)[0]
+
+distances = []
+for product in products:
+  # Compute the cosine distance for each product description
+  dist = distance.cosine(search_embedding, product['embedding'])
+  distances.append(dist)
+
+# Find and print the most similar product short_description    
+min_dist_ind = np.argmin(distances)
+print(products[min_dist_ind]['short_description'])
+
+# omo
