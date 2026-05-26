a7f5f5cdbd9ca594decf9f6bc61a06f745aa54ec
Samuel Adams <sama29571@gmail.com>
2026-05-26 14:30:47 +0000

    chore: add db querying

--- vector_dbs.py
+++ vector_dbs.py
@@ -45,3 +45,20 @@ collection.add(
 # Print the collection size and first ten items
 print(f"No. of documents: {collection.count()}")
 print(f"First ten documents: {collection.peek(10)}")
+
+# Retrieve the netflix_titles collection
+collection = client.get_collection(
+    name="netflix_titles",
+    embedding_function=OpenAIEmbeddingFunction(
+        model_name="text-embedding-3-small",
+        api_key="<OPENAI_API_TOKEN>"
+    )
+)
+
+# Query the collection for "films about dogs"
+result = collection.query(
+    query_texts=["films about dogs"],
+    n_results=3
+)
+
+print(result)
