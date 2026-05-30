ede53b0e627bde4c7cd618b4c4859ea21b443a1b
Samuel Adams <sama29571@gmail.com>
2026-05-30 11:33:25 +0000

    create index

--- pinecone.py
+++ pinecone.py
@@ -1,5 +1,16 @@
-# Import the Pinecone library
+# Import ServerlessSpec
+from pinecone import ServerlessSpec
 from pinecone import Pinecone
-
-# Initialize the Pinecone client
+# Initialize the Pinecone client with your API key
 pc = Pinecone(api_key="pcsk_5dVpiR_Gjgx2xBfPbBPvKZGsd8UXSuTw6fuFn6WaDuCAfpMfiSDQYCUPFwyiVeqgYcfAVr")
+
+# Create your Pinecone index
+pc.delete_index('my-first-index')
+pc.create_index(
+    name="my-first-index",
+    dimension=256,
+    spec=ServerlessSpec(
+        cloud='aws',
+        region='us-east-1'
+    )
+)
