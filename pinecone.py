e47e368c7146d6be4d6c5e957d5a5abfdc7f30ae
Samuel Adams <sama29571@gmail.com>
2026-05-30 14:10:45 +0000

    chore: connect index

--- pinecone.py
+++ pinecone.py
@@ -14,3 +14,13 @@ pc.create_index(
         region='us-east-1'
     )
 )
+
+
+ # Set up the client with your API key
+pc = Pinecone(api_key="pcsk_5dVpiR_Gjgx2xBfPbBPvKZGsd8UXSuTw6fuFn6WaDuCAfpMfiSDQYCUPFwyiVeqgYcfAVr")
+
+# Connect to your index
+index = pc.Index("my-first-index")
+
+# Print the index statistics
+print(index.describe_index_stats())
