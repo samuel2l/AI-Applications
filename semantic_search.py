62c6f892811fa8854331d6178f60e5f763afbf85
Samuel Adams <sama29571@gmail.com>
2026-06-03 18:55:40 +0000

    finish semantic search

--- semantic_search.py
+++ semantic_search.py
@@ -58,3 +58,41 @@ for batch in np.array_split(df, len(df) / batch_limit):
         vectors=zip(ids, embeds, metadatas),
         namespace="squad_dataset"
     )
+
+
+
+# Initialize the Pinecone client
+pc = Pinecone(api_key="pcsk_5dVpiR_Gjgx2xBfPbBPvKZGsd8UXSuTw6fuFn6WaDuCAfpMfiSDQYCUPFwyiVeqgYcfAVr")
+index = pc.Index('pinecone-datacamp')
+
+query = "What is in front of the Notre Dame Main Building?"
+
+# Create the query vector
+
+query_response = client.embeddings.create(
+
+    input=query,
+
+    model="text-embedding-3-small"
+
+)
+
+query_emb = query_response.data[0].embedding
+
+# Query the index and retrieve the top five most similar vectors
+
+retrieved_docs = index.query(
+
+    vector=query_emb,
+
+    top_k=5,
+
+    namespace="squad_dataset"
+
+)
+
+for result in retrieved_docs['matches']:
+
+    print(f"{result['id']}: {round(result['score'], 2)}")
+
+    print('\n')
